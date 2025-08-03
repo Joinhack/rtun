1f1651f9b17459cfdb3b1a524e0591905ad0a11a
Joinhack <joinhack@gmail.com>
2025-08-03 11:44:13 +0800

    set the keepalive time.

--- crates/tun/src/net/sys/mod.rs
+++ crates/tun/src/net/sys/mod.rs
@@ -4,8 +4,36 @@ mod macos;
 #[cfg(target_os = "macos")]
 pub use macos::*;
 
-#[cfg(unix)]
+#[cfg(any(
+    target_os = "android",
+    target_os = "dragonfly",
+    target_os = "freebsd",
+    target_os = "fuchsia",
+    target_os = "illumos",
+    target_os = "ios",
+    target_os = "visionos",
+    target_os = "linux",
+    target_os = "macos",
+    target_os = "netbsd",
+    target_os = "tvos",
+    target_os = "watchos",
+    target_os = "cygwin",
+))]
 mod unix;
 
-#[cfg(unix)]
+#[cfg(any(
+    target_os = "android",
+    target_os = "dragonfly",
+    target_os = "freebsd",
+    target_os = "fuchsia",
+    target_os = "illumos",
+    target_os = "ios",
+    target_os = "visionos",
+    target_os = "linux",
+    target_os = "macos",
+    target_os = "netbsd",
+    target_os = "tvos",
+    target_os = "watchos",
+    target_os = "cygwin",
+))]
 pub use unix::*;
--- crates/tun/src/net/sys/unix.rs
+++ crates/tun/src/net/sys/unix.rs
@@ -1,7 +1,8 @@
-use std::io;
 use std::net::SocketAddr;
+use std::os::fd::{AsRawFd, FromRawFd};
+use std::{io, time::Duration};
 
-use socket2::{Domain, Socket, Type};
+use socket2::{Domain, Protocol, Socket, TcpKeepalive, Type};
 use tokio::net::{TcpSocket, UdpSocket};
 
 use crate::{net::set_ip_bound_if, option::OUTBOUND_INTERFACES};
@@ -22,11 +23,17 @@ pub fn create_outbound_udp_socket(addr: &SocketAddr) -> io::Result<UdpSocket> {
 
 pub fn create_outbound_tcp_socket(addr: &SocketAddr) -> io::Result<TcpSocket> {
     let socket = match *addr {
-        SocketAddr::V4(_) => TcpSocket::new_v4()?,
-        SocketAddr::V6(_) => TcpSocket::new_v6()?,
+        SocketAddr::V4(_) => Socket::new(Domain::IPV4, Type::STREAM, Some(Protocol::TCP))?,
+        SocketAddr::V6(_) => Socket::new(Domain::IPV6, Type::STREAM, Some(Protocol::TCP))?,
     };
     for iface in OUTBOUND_INTERFACES.iter() {
         set_ip_bound_if(&socket, addr, iface)?;
     }
+    let keepalive = TcpKeepalive::new()
+        .with_time(Duration::from_secs(600))
+        .with_interval(Duration::from_secs(60));
+    socket.set_keepalive(true)?;
+    socket.set_tcp_keepalive(&keepalive)?;
+    let socket = unsafe { TcpSocket::from_raw_fd(socket.as_raw_fd()) };
     Ok(socket)
 }
